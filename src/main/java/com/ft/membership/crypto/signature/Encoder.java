845a1c82dc25a6b3f015f3b86dd6b8b6ed5acd0f
Anurag Kapur <anurag@beancrunch.com>
2015-10-02 16:12:58 +0100

    Add javadocs with info on encoding

--- src/main/java/com/ft/membership/crypto/signature/Encoder.java
+++ src/main/java/com/ft/membership/crypto/signature/Encoder.java
@@ -9,11 +9,23 @@ public class Encoder {
     private static final Base64.Encoder BASE_64_ENCODER = Base64.getUrlEncoder().withoutPadding();
     private static final Base64.Decoder BASE_64_DECODER = Base64.getUrlDecoder();
 
+    /**
+     * Get Base64 encoded String of a byte array in UTF-8 charset
+     *
+     * @param bytes
+     * @return
+     */
     public static String getBase64EncodedString(final byte[] bytes) {
 
         return new String(BASE_64_ENCODER.encode(bytes), StandardCharsets.UTF_8);
     }
 
+    /**
+     * Get byte array in UTF-8 charset from Base64 encoded string
+     *
+     * @param encodedString
+     * @return
+     */
     public static Optional<byte[]> getBase64DecodedBytes(final String encodedString) {
 
         try {
